46cbfd0b3e2a4be2cf986e74301c76c042164105
princeraj07m <princerajm31@gmail.com>
2025-09-11 01:12:07 +0530

    main added

--- Dashboard/src/app/app.ts
+++ Dashboard/src/app/app.ts
@@ -1,4 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,16 @@ import { Component, signal } from '@angular/core';
   standalone: false,
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('Dashboard');
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      document.body.style.overflow = 'auto';
+    });
+  }
 }
