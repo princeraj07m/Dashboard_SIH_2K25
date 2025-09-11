8a02fda3f57cd529a451bceb4cb4ae3f9c8a50f2
princeraj07m <princerajm31@gmail.com>
2025-09-11 12:14:41 +0530

    cors fixed

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'http://13.53.114.188:5000/api',
   backendUrl: 'http://13.53.114.188:5000' ,
-  weatherApiKey: 'b6907d289e10d714a6e88b30761fae22'
+  weatherApiKey: 'b6907d289e10d714a6e88b30761fae22',
+  weatherApiUrl: 'https://api.openweathermap.org/data/2.5'
 };  
